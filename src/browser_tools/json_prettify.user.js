8e2f16055ee35bf8701ec3932ff9c76803bb465c
kilfu0701 <kilfu0701@gmail.com>
2014-09-03 11:25:23 +0800

    update json prettify ONLY for json format.

--- src/browser_tools/json_prettify.user.js
+++ src/browser_tools/json_prettify.user.js
@@ -22,12 +22,32 @@ var debug_mode = 1;
         var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
         var t = document.body.textContent;
         var pre = document.createElement('pre');
-
+        
         //console.log(charCode);
+        
+        var my_account = 'yourEmailName@gmail.com';
+        var my_password = 'yourPassword';
+        var my_pin = 'yourPin';
+        
         if(charCode === 1) {
-            document.body.textContent = '';
-            pre.textContent = JSON.stringify(JSON.parse(t), undefined, 4);        
-            document.body.appendChild(pre);
+            if(document.contentType === 'application/json') {
+                document.body.textContent = '';
+                pre.textContent = JSON.stringify(JSON.parse(t), undefined, 4);        
+                document.body.appendChild(pre);
+            } else if(typeof $ !== undefined) {
+                if(location.host === '10.1.201.168' || location.host === '10.1.201.186') {
+                    var $account = $('input#account');
+                    
+                    if($account.size() === 1) {
+                        // auto signin
+                        $account.val(my_account);
+                        $('input#password').val(my_password);
+                    } else if(location.pathname === '/masterpassword/input') {
+                        // auto input pin
+                        $('input#masterpassword').val(my_pin);
+                    }
+                }
+            }
         }
     };
 })(window, document);
