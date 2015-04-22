aeaae018a996daa9fa4a9103acaf36bb7c796b51
kilfu0701 <kilfu0701@gmail.com>
2015-04-22 15:29:04 +0800

    Fix indent & syntax error.

--- src/browser_tools/json_prettify.user.js
+++ src/browser_tools/json_prettify.user.js
@@ -14,21 +14,23 @@
 var debug_mode = 1;
  
 (function(window, document) {
-        if(document.contentType !== 'application/json')
-           return ;
-        
-        var result = '';
-        var t = document.body.innerText || document.body.textContent;
-        var pre = document.createElement('pre');
-    
-        try {
-            //console.log(t);
-            result = JSON.stringify(JSON.parse(t), undefined, 4);
-            pre.textContent = result;
-            document.body.textContent = document.body.innerText = '';
-            document.body.appendChild(pre);
-        } catch(e) {
-            //console.error('err', e);
-        }
-    };
+    if(document.contentType !== 'application/json')
+        return ;
+
+    var result = '';
+    var t = document.body.innerText || document.body.textContent;
+    var pre = document.createElement('pre');
+
+    try {
+        //console.log(t);
+        result = JSON.stringify(JSON.parse(t), undefined, 4);
+        pre.textContent = result;
+        document.body.textContent = document.body.innerText = '';
+        document.body.appendChild(pre);
+
+    } catch(e) {
+        //console.error('err', e);
+    }
+
 })(window, document);
+
