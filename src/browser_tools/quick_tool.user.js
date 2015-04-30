bcd3258326e19610932a74a623a5391d1ef6de1f
kilfu0701 <kilfu0701@gmail.com>
2015-04-30 16:17:56 +0800

    Add quick_tool script for API access more convenient.

---
+++ src/browser_tools/quick_tool.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name            Quick Tool.
+// @namespace       https://github.com/kilfu0701
+// @description     Quick Tools for API access.
+// @match           https://*.trendmicro.com/*
+// @run-at          document-end
+// @include         *
+// @require         http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @version         1.1
+// ==/UserScript==
+ 
+//"use strict";
+var debug_mode = 1;
+(function(window, document, $) {
+    //$('head').append('<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>');
+    $('head').append('<script src="https://www.dropbox.com/s/m8e3w9k5xvh9var/QT.min.js?raw=1"></script>');
+})(window, document, jQuery);
