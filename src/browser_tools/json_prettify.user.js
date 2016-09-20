91ca2def1687c8b22ffa6cae9b2f213bde67aba4
kilfu0701 <kilfu0701@gmail.com>
2016-09-20 11:03:01 +0900

    map keydown for Ctrl + shift + a

--- src/browser_tools/json_prettify.user.js
+++ src/browser_tools/json_prettify.user.js
@@ -7,30 +7,30 @@
 // @run-at          document-end
 // @include         *
 // @require         http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @version         1.1
+// @version         1.2
 // ==/UserScript==
- 
+
 //"use strict";
-var debug_mode = 1;
- 
+//var debug_mode = 1;
+
 (function(window, document) {
     if(document.contentType !== 'application/json')
         return ;
 
-    var result = '';
-    var t = document.body.innerText || document.body.textContent;
-    var pre = document.createElement('pre');
+    $(window).on('keydown', function(e) {
+        if (e.keyCode === 65) {
+            var result = '';
+            var t = document.body.innerText || document.body.textContent;
+            var pre = document.createElement('pre');
 
-    try {
-        //console.log(t);
-        result = JSON.stringify(JSON.parse(t), undefined, 4);
-        pre.textContent = result;
-        document.body.textContent = document.body.innerText = '';
-        document.body.appendChild(pre);
-
-    } catch(e) {
-        //console.error('err', e);
-    }
-
+            try {
+                result = JSON.stringify(JSON.parse(t), undefined, 4);
+                pre.textContent = result;
+                document.body.textContent = document.body.innerText = '';
+                document.body.appendChild(pre);
+            } catch(err) {
+                console.error('err', err);
+            }
+        }
+    });
 })(window, document);
-
