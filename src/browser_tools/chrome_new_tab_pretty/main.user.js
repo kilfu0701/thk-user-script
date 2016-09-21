1f13986ef5f3ad9802583d4ece4abe095ad9f7b3
kilfu0701 <kilfu0701@gmail.com>
2016-09-21 17:14:23 +0900

    update chrome new tab pretty

--- src/browser_tools/chrome_new_tab_pretty/main.user.js
+++ src/browser_tools/chrome_new_tab_pretty/main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chrome New Tab Prettify.
 // @namespace    https://github.com/kilfu0701
-// @version      0.2
+// @version      0.3
 // @description  Prettify UI on Chrome new tab.
 // @author       kilfu0701
 // @match        /^http[s]?:\/\/www.google*/
@@ -11,8 +11,30 @@
 // @grant        none
 // ==/UserScript==
 
-if(location.pathname === "/_/chrome/newtab") {
-    $('.mv-hide').hide();
-    $('#lga').hide();
-    $('#f').hide();
+// 0 = Brower default
+var style = 0;
+
+// logo, search_bar, blocks
+var hide = ['logo', 'search_bar', 'blocks'];
+
+if (location.pathname === "/_/chrome/newtab") {
+    var $el = {
+        blocks: $('.mv-hide'),
+        logo: $('#lga'),
+        search_bar: $('#f')
+    };
+
+    $.each(hide, function (val) {
+        if (val in $el) {
+            $el[val].hide();
+        }
+    });
+
+    switch (style) {
+        case 0:
+        break;
+
+        default:
+        break;
+    }
 }
