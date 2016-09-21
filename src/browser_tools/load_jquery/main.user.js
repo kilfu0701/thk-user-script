aac5aef98cb0ea73877940f511c479c2568462c3
kilfu0701 <kilfu0701@gmail.com>
2016-09-21 23:31:27 +0900

    add new script

---
+++ src/browser_tools/load_jquery/main.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            Load JQuery into page.
+// @namespace       https://github.com/kilfu0701
+// @description     Load jQuery into page if not exists, easy for debugging in console.
+// @match           http://*/*
+// @match           https://*/*
+// @run-at          document-end
+// @include         *
+// @version         1.0
+// ==/UserScript==
+
+var jQuery = {
+    version: '1.12.4'
+};
+
+(function(window, document) {
+
+    if (typeof jQuery === 'undefined') {
+        var jq = document.createElement('script');
+        jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + jQuery.version + "/jquery.min.js";
+        document.getElementsByTagName('head')[0].appendChild(jq);
+    }
+
+})(window, document);
